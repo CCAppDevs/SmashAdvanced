1c725592f19f0bc40a6ce79b8d9cf1267bb67710
Jesse Harlan <jesse.harlan@centralia.edu>
2026-02-12 12:31:56 -0800

    Added basic games controller

--- SmashAdvanced/Data/ApplicationDbContext.cs
+++ SmashAdvanced/Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SmashAdvanced.Models;
 
 namespace SmashAdvanced.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Game> Games { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ SmashAdvanced/Models/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace SmashAdvanced.Models
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Requirements { get; set; } = string.Empty;
+    }
+}
